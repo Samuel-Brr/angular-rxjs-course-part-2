c0e7b28d81c75e0a2495cfb2fac471789bdcaeaa
Samuel-Brr <samuel.barre972@gmail.com>
2022-03-10 18:38:18 +0100

    BehaviorSubject Store - Example of a Data Modification Operation

--- src/app/common/store.service.ts
+++ src/app/common/store.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject, Observable, Subject, timer } from "rxjs";
+import { fromPromise } from "rxjs/internal-compatibility";
 import { delayWhen, map, retryWhen, shareReplay, tap } from "rxjs/operators";
 import { Course } from "../model/course";
 import { createHttpObservable } from "./util";
@@ -9,6 +10,30 @@ import { createHttpObservable } from "./util";
 })
 
 export class Store {
+  saveCourse(courseId: number, changes: any): Observable<any> {
+
+    const courses = this.subject.getValue()
+
+    const courseIndex = courses.findIndex(course => course.id == courseId)
+
+    const newCourses = courses.slice(0)
+
+    newCourses[courseIndex] = {
+      ...courses[courseIndex],
+      ...changes
+    }
+
+    this.subject.next(newCourses)
+
+
+    return fromPromise(fetch(`/api/courses/${courseId}`, {
+      method: 'PUT',
+      body: JSON.stringify(changes),
+      headers: {
+        'content-type' : 'application/json'
+      }
+    }))
+  }
 
   selectBeginnerCourses(): Observable<Course[]> {
     return this.filterByCategorie("BEGINNER")
